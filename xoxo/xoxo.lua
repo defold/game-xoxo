909e343274098dc675e6e19c0c2dd2a2b0a94a8f
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-01-27 22:35:39 +0100

    Simplified client-server flow

--- xoxo/xoxo.lua
+++ xoxo/xoxo.lua
@@ -15,54 +15,61 @@ function M.on_join_match(fn)
 end
 function M.join_match(callback)
 	callback = wrap(callback)
-	on_join_match_fn(function()
-		callback()
+	on_join_match_fn(function(success, message)
+		assert(success ~= nil)
+		assert(success or mesage)
+		callback(success, message)
 	end)
 end
 
-local on_request_match_state_fn
-function M.on_request_match_state(fn)
-	on_request_match_state_fn = fn
+local match_update = nil
+local on_match_update_fn
+-- from server to client
+function M.match_update(state, active_player, other_player, your_turn)
+	assert(state)
+	assert(active_player)
+	assert(other_player)
+	assert(your_turn ~= nil)
+	match_update = {
+		state = state,
+		active_player = active_player,
+		other_player = other_player,
+		your_turn = your_turn,
+	}
+	if on_match_update_fn then
+		on_match_update_fn(state, active_player, other_player, your_turn)
+	end
 end
-function M.request_match_state(callback)
-	callback = wrap(callback)
-	on_request_match_state_fn(function(state, active_player, other_player, your_turn)
-		assert(state)
-		assert(active_player)
-		assert(other_player)
-		assert(your_turn ~= nil)
-		callback(state, active_player, other_player, your_turn)
-	end)
+-- client to server
+function M.on_match_update(callback)
+	on_match_update_fn = wrap(callback)
+	if match_update then
+		on_match_update_fn(
+			match_update.state,
+			match_update.active_player,
+			match_update.other_player,
+			match_update.your_turn)
+	end
 end
 
 local on_send_player_move_fn
+-- server to client
 function M.on_send_player_move(fn)
 	on_send_player_move_fn = fn
 end
-function M.send_player_move(row, column, callback)
-	callback = wrap(callback)
-	on_send_player_move_fn(row, column, function(state, active_player, other_player, your_turn)
-		assert(state)
-		assert(active_player)
-		assert(other_player)
-		assert(your_turn ~= nil)
-		callback(state, active_player, other_player, your_turn)
-	end)
+-- client to server
+function M.send_player_move(row, column)
+	on_send_player_move_fn(row, column)
 end
 
 local on_request_rematch_fn
+-- server to client
 function M.on_request_rematch(fn)
 	on_request_rematch_fn = fn
 end
-function M.request_rematch(callback)
-	callback = wrap(callback)
-	on_request_rematch_fn(function(state, active_player, other_player, your_turn)
-		assert(state)
-		assert(active_player)
-		assert(other_player)
-		assert(your_turn ~= nil)
-		callback(state, active_player, other_player, your_turn)
-	end)
+-- client to server
+function M.request_rematch()
+	on_request_rematch_fn()
 end
 
 
