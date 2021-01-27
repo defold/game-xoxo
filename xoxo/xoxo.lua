6b3fb255c2ea330d40204f15f785f69868ad16f1
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-01-27 21:53:38 +0100

    Validate callback data and send if it is the players turn or not

--- xoxo/xoxo.lua
+++ xoxo/xoxo.lua
@@ -14,7 +14,10 @@ function M.on_join_match(fn)
 	on_join_match_fn = fn
 end
 function M.join_match(callback)
-	on_join_match_fn(wrap(callback))
+	callback = wrap(callback)
+	on_join_match_fn(function()
+		callback()
+	end)
 end
 
 local on_request_match_state_fn
@@ -22,7 +25,14 @@ function M.on_request_match_state(fn)
 	on_request_match_state_fn = fn
 end
 function M.request_match_state(callback)
-	on_request_match_state_fn(wrap(callback))
+	callback = wrap(callback)
+	on_request_match_state_fn(function(state, active_player, other_player, your_turn)
+		assert(state)
+		assert(active_player)
+		assert(other_player)
+		assert(your_turn ~= nil)
+		callback(state, active_player, other_player, your_turn)
+	end)
 end
 
 local on_send_player_move_fn
@@ -30,7 +40,14 @@ function M.on_send_player_move(fn)
 	on_send_player_move_fn = fn
 end
 function M.send_player_move(row, column, callback)
-	on_send_player_move_fn(row, column, wrap(callback))
+	callback = wrap(callback)
+	on_send_player_move_fn(row, column, function(state, active_player, other_player, your_turn)
+		assert(state)
+		assert(active_player)
+		assert(other_player)
+		assert(your_turn ~= nil)
+		callback(state, active_player, other_player, your_turn)
+	end)
 end
 
 local on_request_rematch_fn
@@ -38,7 +55,14 @@ function M.on_request_rematch(fn)
 	on_request_rematch_fn = fn
 end
 function M.request_rematch(callback)
-	on_request_rematch_fn(wrap(callback))
+	callback = wrap(callback)
+	on_request_rematch_fn(function(state, active_player, other_player, your_turn)
+		assert(state)
+		assert(active_player)
+		assert(other_player)
+		assert(your_turn ~= nil)
+		callback(state, active_player, other_player, your_turn)
+	end)
 end
 
 
