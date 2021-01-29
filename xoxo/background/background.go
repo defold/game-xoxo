ecbafc5ebcd916603bd5dc3a4ddf16201b722789
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-01-29 08:33:01 +0100

    Added background and splash

---
+++ xoxo/background/background.go
@@ -0,0 +1,15 @@
+components {
+  id: "bg"
+  component: "/xoxo/background/background.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
